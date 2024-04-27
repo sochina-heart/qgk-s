a043b95fb7e22a87888d6f5df9cea35a254408af
heart-sochina <3014862187@qq.com>
2024-04-27 19:07:26 +0800

    add column

--- src/main/kotlin/com/sochina/demo/utils/encrypt/gm/sm4/Sm4Utils.kt
+++ src/main/kotlin/com/sochina/demo/utils/encrypt/gm/sm4/Sm4Utils.kt
@@ -1,16 +1,18 @@
 package com.sochina.demo.utils.encrypt.gm.sm4
 
+import com.sochina.demo.utils.Base64Utils
 import org.bouncycastle.jce.provider.BouncyCastleProvider
 import org.bouncycastle.util.encoders.Hex
 import java.security.AlgorithmParameters
 import java.security.Key
 import java.security.SecureRandom
 import java.security.Security
-import java.util.Arrays
+import java.util.*
 import javax.crypto.Cipher
 import javax.crypto.KeyGenerator
 import javax.crypto.spec.IvParameterSpec
 import javax.crypto.spec.SecretKeySpec
+import kotlin.io.encoding.Base64
 
 object SM4Utils {
     private val ENCODING: String = Sm4Helper.ENCODING.msg
@@ -254,6 +256,58 @@ object SM4Utils {
             null
         } else {
             decryptEcb(KEY, paramStr)
+        }
+    }
+
+    fun encrypt(data: String, key: String, iv: String): String? {
+        try {
+            //"算法/模式/补码方式"NoPadding PkcsPadding
+            val cipher = Cipher.getInstance("AES/CBC/NoPadding")
+            val blockSize = cipher.blockSize
+
+            val dataBytes = data.toByteArray()
+            var plaintextLength = dataBytes.size
+            if (plaintextLength % blockSize != 0) {
+                plaintextLength = plaintextLength + (blockSize - (plaintextLength % blockSize))
+            }
+
+            val plaintext = ByteArray(plaintextLength)
+            System.arraycopy(dataBytes, 0, plaintext, 0, dataBytes.size)
+
+            val keyspec = SecretKeySpec(key.toByteArray(), "AES")
+            val ivspec = IvParameterSpec(iv.toByteArray())
+
+            cipher.init(Cipher.ENCRYPT_MODE, keyspec, ivspec)
+            val encrypted = cipher.doFinal(plaintext)
+
+            return org.bouncycastle.util.encoders.Base64.toBase64String(encrypted)
+        } catch (e: java.lang.Exception) {
+            e.printStackTrace()
+            return null
+        }
+    }
+
+    /**
+     * 解密方法
+     *
+     * @param data 要解密的数据
+     * @param key  解密key
+     * @param iv   解密iv
+     * @return 解密的结果
+     */
+    fun desEncrypt(data: String?, key: String, iv: String): String? {
+        try {
+            val encrypted1: ByteArray? = Base64Utils.decode(data)
+
+            val cipher = Cipher.getInstance("AES/CBC/NoPadding")
+            val keySpec = SecretKeySpec(key.toByteArray(), "AES")
+            val ivSpec = IvParameterSpec(iv.toByteArray())
+            cipher.init(Cipher.DECRYPT_MODE, keySpec, ivSpec)
+            val original = cipher.doFinal(encrypted1)
+            return String(original).trim { it <= ' ' }
+        } catch (e: java.lang.Exception) {
+            e.printStackTrace()
+            return null
         }
     }
 }
